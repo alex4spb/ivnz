9e02bef754803202f89af9a0b090770f5bca2222
Ekaterina Kondrashova <kytty@list.ru>
2018-06-06 17:54:22 +0300

    top menu animation

--- dev/js/script.js
+++ dev/js/script.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function($) {
         var a = $(this),
             hash = a.attr("href"),
             target = $('[id="' + hash.substr(1) + '"]');
-        if (target.length != 0){
-             $("html:not(:animated), body:not(:animated)").animate(
+        if (target.length != 0) {
+            $("html:not(:animated), body:not(:animated)").animate(
                 {
                     scrollTop: target.offset().top
                 },
@@ -17,28 +17,34 @@ jQuery(document).ready(function($) {
                 }
             );
         }
-       
     });
     AOS.init({
         duration: 1200,
         once: true
     });
-    $(function() {
-        var $el,
-            leftPos,
-            newWidth,
-            $mainNav = $(".topnav__menu");
+    if ($(document).width() > 767)
+        $(function() {
+            var $el,
+                leftPos,
+                newWidth,
+                $mainNav = $(".topnav__menu#magic");
 
-        $mainNav.append("<div id='magic-line'></div>");
-        var $magicLine = $("#magic-line");
+            $mainNav.append("<div id='magic-line'></div>");
+            var $magicLine = $("#magic-line");
 
-        $magicLine
-            .width($(".topnav__menu-item.active").width())
-            .css("left", $(".topnav__menu-item.active").position().left)
-            .data("origLeft", $magicLine.position().left)
-            .data("origWidth", $magicLine.width());
-        $(".topnav__menu .topnav__menu-item a").focusin(
-            function() {
+            $magicLine
+                .width(
+                    $(".topnav__menu#magic .topnav__menu-item.active").width()
+                )
+                .css(
+                    "left",
+                    $(
+                        ".topnav__menu#magic .topnav__menu-item.active"
+                    ).position().left
+                )
+                .data("origLeft", $magicLine.position().left)
+                .data("origWidth", $magicLine.width());
+            $(".topnav__menu#magic .topnav__menu-item a").focusin(function() {
                 $el = $(this);
                 leftPos = $el.parent().position().left;
                 newWidth = $el.parent().width();
@@ -46,15 +52,40 @@ jQuery(document).ready(function($) {
                     left: leftPos,
                     width: newWidth
                 });
-            },
-        );
-         $(".topnav__menu .topnav__menu-item a").focusout(
-            function() {
+            });
+            $(".topnav__menu#magic .topnav__menu-item a").focusout(function() {
                 $magicLine.stop().animate({
                     left: $magicLine.data("origLeft"),
                     width: $magicLine.data("origWidth")
                 });
-            }
-        );        
-    });
+            });
+        });
+    else
+        $(function() {
+            var pull = $("#pull");
+            menu = $(".topnav__menu.-mobile");
+            menuHeight = menu.height();
+            close = $(".topnav__menu-close img");
+
+            $(pull).on("click", function(e) {
+                e.preventDefault();
+                menu.animate(
+                    {
+                        left: "0px"
+                    },
+                    200
+                );
+                setTimeout(function() { menu.addClass('active'); }, 500);
+            });
+            $(close).on("click", function(e) {
+                e.preventDefault();
+                menu.animate(
+                    {
+                        left: "-100%"
+                    },
+                    200
+                );
+                setTimeout(function() { menu.removeClass("active"); }, 500);                
+            });
+        });
 });
